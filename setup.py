73fb52bda1e1eb8c6ed8bd86d9f013b694d97197
György Demarcsek <gyorgy.demarcsek@prezi.com>
2017-08-05 20:29:38 +0200

    single module package

--- setup.py
+++ setup.py
@@ -54,13 +54,7 @@ setup(
     # What does your project relate to?
     keywords='apparmor',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
+    py_modules=["apparmor_light"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
